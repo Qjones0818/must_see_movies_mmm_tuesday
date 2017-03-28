7c1391b8638f1b545d4a197370115105e4910abd
Raghu Betina <raghu@firstdraft.com>
2017-03-28 15:57:34 +0000

    Generate movies resource with starter:resource

---
+++ db/migrate/20170328155734_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :director_id
+      t.string :year
+      t.string :image
+
+      t.timestamps
+
+    end
+  end
+end
